0d99235c26fb6ea6dfb494072fd7724563ddb7f1
David Rodriguez <davidgr80@gmail.com>
2017-10-03 16:57:39 -0300

    dia 3/10 16:57

--- api-restful-favoritos/index.js
+++ api-restful-favoritos/index.js
@@ -1,1 +1,18 @@
-.
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3678;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/prueba', function(req, res){
+    res.send({message: "Hola mundo"});
+})
+
+app.listen(port,function(){
+    console.log('Escuchando en http://localhost:3678');
+});
