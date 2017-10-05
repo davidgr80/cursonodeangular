d9017b6e0355dad61e59deb49f90d6509a49c4eb
David Rodriguez <davidgr80@gmail.com>
2017-10-05 11:31:56 -0300

    Terminada la api rest

--- api-restful-favoritos/controllers/favorito.js
+++ api-restful-favoritos/controllers/favorito.js
@@ -3,24 +3,71 @@
  */
 'use strict'
 
+var Favorito = require('../models/favorito');
+
 function getFavorito(req, res) {
     var favoritoId = req.params.id;
-    res.status(200).send({data: favoritoId});
+    Favorito.findById(favoritoId,(err, favorito) => {
+        if(err){
+            res.status(500).send({message: 'Error al listar el favorito'});
+        } else {
+            res.status(200).send({favorito});
+        }
+    })
 }
 function getFavoritos(req, res) {
-
+    Favorito.find({},(err,favoritos) => {
+        if(err){
+            res.status(500).send({message: 'Error al listar los favoritos'});
+        } else {
+            res.status(200).send({favoritos});
+        }
+    })
 }
+
 function saveFavorito(req, res) {
+    var favorito = new Favorito();
     var params = req.body;
-    res.status(200).send({favorito: params});
+    favorito.title = params.title;
+    favorito.description = params.description;
+    favorito.url = params.url;
+
+    favorito.save((err,favoritoStored) => {
+        if(err){
+            res.status(500).send({message: 'Error al guardar el favorito'});
+        } else {
+            res.status(200).send({favorito: favoritoStored});
+        }
+    })
 }
 function updateFavorito(req, res) {
-    var params = req.body;
-    res.status(200).send({favorito: params});
+    var favoritoId = req.params.id;
+    var update = req.body;
+    Favorito.findByIdAndUpdate(favoritoId, update, (err, favoritoUpdated) => {
+        if(err){
+            res.status(500).send({message: 'Error al actualizar el favorito'});
+        } else {
+            res.status(200).send({favoritoUpdated});
+        }
+    })
 }
+
 function deleteFavorito(req, res) {
     var favoritoId = req.params.id;
-    res.status(200).send({data: favoritoId});
+
+    Favorito.findById(favoritoId, (err,favorito) => {
+        if(err) {
+            res.status(500).send({message: 'Error al borrar favorito'});
+        } else {
+            favorito.remove((err) => {
+                if(err){
+                    res.status(500).send({message: 'No se pudo eliminar el favorito'});
+                } else {
+                    res.status(200).send({message: 'El favorito fue eliminado con exito'});
+                }
+            });
+        }
+    });
 }
 
 
--- api-restful-favoritos/routes/favorito.js
+++ api-restful-favoritos/routes/favorito.js
@@ -10,7 +10,7 @@ var api = express.Router();
 api.get('/favorito/:id', FavoritoController.getFavorito);
 api.get('/favoritos', FavoritoController.getFavoritos);
 api.post('/favorito', FavoritoController.saveFavorito);
-api.put('/favorito', FavoritoController.updateFavorito);
+api.put('/favorito/:id', FavoritoController.updateFavorito);
 api.delete('/favorito/:id', FavoritoController.deleteFavorito);
 
 module.exports = api;
